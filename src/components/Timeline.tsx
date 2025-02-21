ef39c8b423e4e3f9faf0418e4c73a96979d4c71b
Fabricio <fepafumi97@gmail.com>
2025-02-21 16:40:54 -0300

    feat: :sparkles: Estilado de Header y cambios de colores

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -10,11 +10,11 @@ interface TimelineProps {
 
 export const Timeline = ({ fechas }: TimelineProps) => {
 	return (
-		<ol className="relative border-s-2 border-red-700">
+		<ol className="relative border-s-2 border-cyan-600">
 			{fechas.map((fecha) => {
 				return (
-					<li className="mb-10 ms-4">
-						<div className="absolute w-3 h-3 bg-red-700 rounded-full mt-1.5 -start-1.5 border border-red-700  hover:scale-125 transition-all"></div>
+					<li className="mb-10 ms-4" key={"fecha"+fecha.titulo}>
+						<div className="absolute w-3 h-3 bg-cyan-600 rounded-full mt-1.5 -start-1.5 border border-cyan-600  hover:scale-125 transition-all"></div>
 						<time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
 							{fecha.fecha}
 						</time>
